d574baedff5562e9b4f2d4b8b540fd2ee0955233
Artur <ArthurZ43@yandex.ru>
2023-03-25 12:28:41 +0300

    create model

--- app/models.py
+++ app/models.py
@@ -1,3 +1,7 @@
 from django.db import models
 
-# Create your models here.
+
+class MenuItem(models.Model):
+    name = models.CharField(max_length=255)
+    url = models.CharField(max_length=255)
+    parent = models.ForeignKey('self', on_delete=models.CASCADE, null=True, blank=True)
