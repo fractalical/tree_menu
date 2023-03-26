1a725fc1165643c7b4486e482ad7a8e2cf375264
Artur <ArthurZ43@yandex.ru>
2023-03-26 20:06:55 +0300

    настроена валидация модели

--- app/admin.py
+++ app/admin.py
@@ -1,9 +1,39 @@
 from django.contrib import admin
 
 # Register your models here.
+from django import forms
 from app.models import MenuItem
+
+
+class MenuItemForm(forms.ModelForm):
+    class Meta:
+        model = MenuItem
+        fields = '__all__'
+
+    def clean(self):
+
+        cleaned_data = super().clean()
+        obj = cleaned_data['name']
+        parent = cleaned_data['parent']
+        while parent is not None:
+            if parent.name == obj:
+                raise forms.ValidationError('Ошибка: рекурсия уровней')
+            else:
+                parent = parent.parent
+
+        return cleaned_data
 
 
 @admin.register(MenuItem)
 class MenuItemAdmin(admin.ModelAdmin):
-    pass
+    list_display = ('name', 'parent', 'level')
+    readonly_fields = ('level',)
+    form = MenuItemForm
+
+    def save_model(self, request, obj, form, change):
+
+        if obj.parent is not None:
+            print('add level')
+            obj.level = obj.parent.level + 1
+
+        super().save_model(request, obj, form, change)
--- app/models.py
+++ app/models.py
@@ -2,8 +2,9 @@ from django.db import models
 
 
 class MenuItem(models.Model):
-    name = models.CharField(max_length=255)
+    name = models.CharField(max_length=255, unique=True)
     parent = models.ForeignKey('self', on_delete=models.CASCADE, null=True, blank=True)
+    level = models.IntegerField(default=0)
 
     def __str__(self):
         return self.name
